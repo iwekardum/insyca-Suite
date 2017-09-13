a682d5c23d18536aeecd9f04e966d7877442552e
Iwe Kardum <kardum-iw@outlook.com>
2017-09-13 13:41:19 +0200

    ik

--- inSyca.bt.global/insyca.bt.gl.pipelines/remove_nil_and_empty_nodes.btp.cs
+++ inSyca.bt.global/insyca.bt.gl.pipelines/remove_nil_and_empty_nodes.btp.cs
@@ -27,7 +27,7 @@ namespace inSyca.bt.gl.pipelines
 "stageId=\"9d0e4108-4cce-4536-83fa-4a5040674ad6\" />      <Components />    </Stage>  </Stages></Docume"+
 "nt>";
         
-        private const string _versionDependentGuid = "bb3f5706-d96b-477c-b65c-ca0d57bbd8bb";
+        private const string _versionDependentGuid = "bd47a356-4768-43b5-ad11-e3ca041e744b";
         
         public remove_nil_and_empty_nodes()
         {
