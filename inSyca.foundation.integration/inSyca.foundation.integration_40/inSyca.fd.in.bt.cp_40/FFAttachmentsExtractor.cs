5f20dcf8a69ca6270ccd1b7c42559a600d425b60
iwe.kardum <kardum-iw@outlook.com>
2017-10-06 17:28:23 +0200

    ik

--- inSyca.foundation.integration/inSyca.foundation.integration_40/inSyca.fd.in.bt.cp_40/FFAttachmentsExtractor.cs
+++ inSyca.foundation.integration/inSyca.foundation.integration_40/inSyca.fd.in.bt.cp_40/FFAttachmentsExtractor.cs
@@ -8,17 +8,29 @@
     using Microsoft.BizTalk.Component;
     using System.Runtime.InteropServices;
     using System.Drawing;
-    using Microsoft.BizTalk.Component;
+    using System.Text.RegularExpressions;
+    using diagnostics;
+    using System.ComponentModel;
 
     [ComponentCategory(CategoryTypes.CATID_PipelineComponent)]
     [ComponentCategory(CategoryTypes.CATID_DisassemblingParser)]
-    [Guid("01F04E58-7AD8-42EF-A3CB-D939B960E8F9")]
-    public class FFAttachmentsExtractor : FFDasmComp, IBaseComponent, IComponentUI, IDisassemblerComponent
+    [Guid("16D92957-2034-4853-840B-BD424C596247")]
+    public class FFAttachmentsExtractor : FFDasmComp, IBaseComponent, IComponentUI, IDisassemblerComponent, IPersistPropertyBag
     {
+        private string regExReplacement = "\".*?\"";
+
+        [Browsable(true)]
+        public string RegExReplacement
+        {
+            get { return regExReplacement; }
+            set { regExReplacement = value; }
+        }
+
         #region IBaseComponent Members
         /// <summary>
         /// Gets Description of the component
         /// </summary>   
+        [Browsable(false)]
         public new string Description
         {
             get
@@ -30,6 +42,7 @@
         /// <summary>
         /// Gets Name of the component
         /// </summary>   
+        [Browsable(false)]
         public new string Name
         {
             get
@@ -41,12 +54,69 @@
         /// <summary>
         /// Gets Version of the component
         /// </summary>
+        [Browsable(false)]
         public new string Version
         {
             get
             {
                 return "1.0";
             }
+        }
+        #endregion
+
+        #region IPersistPropertyBag
+
+        /// <summary>
+        /// Gets class ID of component for usage from unmanaged code.
+        /// </summary>
+        /// <param name="classid">Class ID of the component.</param>
+        public new void GetClassID(out Guid classid)
+        {
+            Log.DebugFormat("GetClassID(out Guid classid)");
+
+            classid = new System.Guid("16D92957-2034-4853-840B-BD424C596247");
+        }
+
+        /// <summary>
+        /// Not implemented.
+        /// </summary>
+        public new void InitNew()
+        {
+        }
+
+        public new void Load(IPropertyBag propertyBag, int errorLog)
+        {
+            Log.DebugFormat("Load(IPropertyBag propertyBag {0} , int errorLog {1})", propertyBag, errorLog);
+
+            base.Load(propertyBag, errorLog);
+
+            using (DisposableObjectWrapper wrapper = new DisposableObjectWrapper(propertyBag))
+            {
+                object val = null;
+                val = PropertyHelper.ReadPropertyBag(propertyBag, "RegExReplacement");
+                if (val != null)
+                {
+                    regExReplacement = (string)val;
+                }
+            }
+
+            Log.DebugFormat("Load RegExReplacement {0}", regExReplacement);
+        }
+
+        public new void Save(IPropertyBag propertyBag, bool clearDirty, bool saveAllProperties)
+        {
+            Log.DebugFormat("Load(IPropertyBag propertyBag {0}, bool clearDirty {1}, bool saveAllProperties {2})", propertyBag, clearDirty, saveAllProperties);
+
+            base.Save(propertyBag, clearDirty, saveAllProperties);
+
+            using (DisposableObjectWrapper wrapper = new DisposableObjectWrapper(propertyBag))
+            {
+                object val = null;
+                val = regExReplacement;
+                propertyBag.Write("RegExReplacement", ref val);
+            }
+
+            Log.DebugFormat("Save RegExReplacement {0}", regExReplacement);
         }
 
         #endregion
@@ -75,7 +145,7 @@
         /// <summary>
         /// Returns messages resulting from the disassemble method execution
         /// </summary>
-        /// <param name="pc">the pipeline context</param>
+        /// <param name="pipelineContext">the pipeline context</param>
         /// <returns></returns>
         //public new IBaseMessage GetNext(IPipelineContext pc)
         //{
@@ -91,30 +161,35 @@
         /// <summary>
         /// called by the messaging engine when a new message arrives
         /// </summary>
-        /// <param name="pc">the pipeline context</param>
-        /// <param name="inmsg">the actual message</param>
-        public new void Disassemble(Microsoft.BizTalk.Component.Interop.IPipelineContext pc, Microsoft.BizTalk.Message.Interop.IBaseMessage inmsg)
+        /// <param name="pipelineContext">the pipeline context</param>
+        /// <param name="inMsg">the actual message</param>
+        public new void Disassemble(Microsoft.BizTalk.Component.Interop.IPipelineContext pipelineContext, Microsoft.BizTalk.Message.Interop.IBaseMessage inMsg)
         {
+            Log.DebugFormat("Execute(IPipelineContext pContext {0}, IBaseMessage pInMsg {1})", pipelineContext, inMsg);
+
             var partName = string.Empty;
             // we start from index 1 because index zero contains the body of the message
             // which we are not interested
-            for (int i = 1; i < inmsg.PartCount; i++)
+            for (int i = 1; i < inMsg.PartCount; i++)
             {
-                IBaseMessagePart currentPart = inmsg.GetPartByIndex(i, out partName);
+                Log.DebugFormat("PartCounter {0}", i);
+
+                IBaseMessagePart currentPart = inMsg.GetPartByIndex(i, out partName);
 
                 Stream currentPartStream = currentPart.GetOriginalDataStream();
-                var ms = new MemoryStream();
-                IBaseMessage outMsg;
-                outMsg = pc.GetMessageFactory().CreateMessage();
 
-                for (int j = 0; j < inmsg.Context.CountProperties; j++)
+                IBaseMessage outMsg;
+                outMsg = pipelineContext.GetMessageFactory().CreateMessage();
+
+                for (int j = 0; j < inMsg.Context.CountProperties; j++)
                 {
+
                     string currentName;
                     string currentNamespace;
-                    object obj = inmsg.Context.ReadAt(j, out currentName, out currentNamespace);
+                    object obj = inMsg.Context.ReadAt(j, out currentName, out currentNamespace);
                     outMsg.Context.Write(currentName, currentNamespace, obj);
 
-                    if (inmsg.Context.IsPromoted(currentName, currentNamespace))
+                    if (inMsg.Context.IsPromoted(currentName, currentNamespace))
                     {
                         outMsg.Context.Promote(currentName, currentNamespace, obj);
                     }
@@ -125,16 +200,50 @@
                     continue;
                 }
 
-                currentPartStream.CopyTo(ms);
+                Stream ms;
+
+                Log.DebugFormat("regExReplacement {0}", regExReplacement);
+
+                if (string.IsNullOrEmpty(regExReplacement))
+                {
+                    Log.DebugFormat("No RegExReplacement");
+
+                    ms = new MemoryStream();
+                    currentPartStream.CopyTo(ms);
+                }
+                else
+                {
+                    StreamReader sr = new StreamReader(currentPartStream);
+
+                    string messageString = sr.ReadToEnd();
+
+                    Log.DebugFormat("messageString before processing{0}", messageString);
+
+                    messageString = messageString.TrimEnd('\r', '\n');
+                    messageString = messageString + Environment.NewLine;
+
+                    //                    Regex rgx = new Regex("\".*?\"");
+                    Regex rgx = new Regex(RegExReplacement);
+
+                    messageString = rgx.Replace(messageString, "");
+
+                    Log.DebugFormat("messageString after processing{0}", messageString);
+
+                    byte[] byteArray = Encoding.UTF8.GetBytes(messageString);
+
+                    ms = new MemoryStream(byteArray);
+                }
 
                 ms.Seek(0, SeekOrigin.Begin);
-                outMsg.AddPart("Body", pc.GetMessageFactory().CreateMessagePart(), true);
+                outMsg.AddPart("Body", pipelineContext.GetMessageFactory().CreateMessagePart(), true);
                 outMsg.BodyPart.Data = ms;
-                
+
                 //Promote attachment file name
                 outMsg.Context.Write("ReceivedFileName", "http://schemas.microsoft.com/BizTalk/2003/file-properties", currentPart.PartProperties.Read("FileName", "http://schemas.microsoft.com/BizTalk/2003/mime-properties"));
 
-                base.Disassemble(pc, outMsg);
+                base.Disassemble(pipelineContext, outMsg);
+
+                Log.DebugFormat("Flatfile Dissasembler(IPipelineContext pContext {0}, IBaseMessage pInMsg {1})", pipelineContext, outMsg);
 
                 //_msgs.Enqueue(outMsg);
             }
